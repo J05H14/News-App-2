df2e1a5fb3e4c5dd26e1896e50372378c54424c3
JAUSH <j05h14@yahoo.com>
2018-11-19 20:30:30 -0800

    Created NewsRoomDatabase

--- app/src/main/java/com/example/rkjc/news_app_2/NewsItemRepository.java
+++ app/src/main/java/com/example/rkjc/news_app_2/NewsItemRepository.java
@@ -5,12 +5,23 @@ import android.arch.lifecycle.LiveData;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
 public class NewsItemRepository {
+    protected List<NewsItem> newsItems;
+
+
     private NewsItemDao mNewsDao;
     private LiveData<List<NewsItem>> mAllNewsItems;
+
+    public NewsItemRepository(Application application){
+        NewsRoomDatabase db = NewsRoomDatabase.getDatabase(application.getApplicationContext());
+        mNewsDao = db.newsDao();
+        mAllNewsItems = mNewsDao.loadAllNewsItems();
+    }
 
     private static class insertAsyncTask extends AsyncTask<NewsItem, Void, Void> {
         private NewsItemDao mAsyncTaskDao;
@@ -22,6 +33,30 @@ public class NewsItemRepository {
         protected Void doInBackground(final NewsItem... params) {
             mAsyncTaskDao.insert(Arrays.asList(params));
             return null;
+        }
+    }
+    private class NewsQueryTask extends AsyncTask<URL, Void, String>{
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(URL... urls){
+            String results = "";
+            try{
+                results = NetworkUtils.getResponseFromHttpUrl(urls[0]);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return results;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            newsItems = JsonUtils.parseNews(s);
+            mNewsDao.insert(newsItems);
         }
     }
 }
